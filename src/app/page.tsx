d79039995764cb80e670ddd7e955f813a8927f8e
george <ccchang0661@gmail.com>
2023-05-26 20:24:04 +0800

    UPD FE: Add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,20 @@ import profile from "../../public/imgs/profile.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container m-auto  pl-2 pr-2">
-      <nav className="fixed">
-        <div className="container">
-          <ul>
-            <li>Project</li>
-            <li>LinkedIN</li>
+    <main className="flex min-h-screen flex-col container m-auto  pl-2 pr-2 text-emerald-500">
+      <nav className="fixed w-full bg-slate-800 left-0 z-50 bg-opacity-50">
+        <div className="container flex justify-between p-2 m-auto">
+          <div className="font-mono font-extrabold">GEORGE CHANG</div>
+          <ul className="flex">
+            <li className="ml-2">Ploject</li>
+            <li className="ml-2">Linkedin</li>
           </ul>
         </div>
       </nav>
       <section className="hero relative flex flex-col sm:flex-row sm:mt-20 justify-center mb-2">
         <div className="flex-shrink-0 w-1/2 flex justify-center items-center absolute sm:relative sm:w-1/2">
           <div className="absoulte text-xl sm:text-3xl">
-            HELLO I AM GEORGE
+            HELLO! I AM GEORGE
             <br />
             <p>Software Engineer</p>
           </div>
@@ -30,43 +31,72 @@ export default function Home() {
       </section>
       <section className="experience-section">
         <h2 className="text-2xl uppercase">experiences</h2>
-        <hr className="mt-2 mb-2" />
-        <div className="text-xl border-l-4 border-cyan-800 pl-4">
-          <h3>
-            Garena <span>01/2021 - Present</span>
-          </h3>
-          <p>Senior Software Engineer(Frontend)</p>
+        <hr className="mt-4 mb-4" />
+        <div className="work-experience mb-8">
+          <div className="text-xl font-bold border-l-4 border-cyan-800 pl-4">
+            <h3 className="flex justify-between text-2xl ">
+              Garena <span className=" font-normal">01/2021 - Present</span>
+            </h3>
+            <p>Senior Software Engineer(Frontend)</p>
+          </div>
+          <div className="mt-2 text-lg">
+            <ul className="list-disc pl-8">
+              <li>
+                Collaborated with multinational team partners to complete 23
+                cross-device projects serving millions of players around the
+                world in more than 10 languages using Vue and Typescript.
+              </li>
+              <li>
+                Save construction time by 60% and increase development
+                experience satisfaction by upgrading(webpack -{">"} vite) and
+                maintaining the front-end template with colleagues.
+              </li>
+              <li>
+                Built the {"game's"} official website using the server-side
+                rendering framework (Nuxt3) to improve SEO and performance.
+              </li>
+              <li>
+                Boost productivity at least 5% by contributing custom hooks and
+                components to the team.
+              </li>
+              <li>
+                Enhanced product robustness by Conducting code reviews with
+                colleagues and implementing end-to-end testing with Cypress.
+              </li>
+            </ul>
+            <div className="tech-stacks"></div>
+          </div>
         </div>
-        <div className="mt-2 text-lg">
-          <ul className="list-disc pl-8">
-            <li>
-              Use Vue and Typescript for front-end development, cooperate with
-              multinational team partners to complete 23 cross-device projects,
-              and serve millions of players around the world in more than 10
-              languages.
-            </li>
-            <li>
-              Upgrade and maintain the front-end template with colleagues, save
-              construction time by 60%, increase the satisfaction of development
-              experience by at least 20%.
-            </li>
-            <li>
-              Use the server-side rendering framework (Nuxt3) to build the
-              game&apos;s offcial website to improve SEO and performance.
-            </li>
-            <li>
-              Boost productivity by contributing custom hooks and components to
-              the team.
-            </li>
-            <li>
-              Participate in code reviews to help junior engineers find blind
-              spots in their code.
-            </li>
-            <li>
-              Enhance product robustness by implementing end-to-end testing with
-              cypress.
-            </li>
-          </ul>
+        <div className="work-experience mb-2">
+          <div className="text-xl font-bold border-l-4 border-cyan-800 pl-4">
+            <h3 className="flex justify-between text-2xl ">
+              Ljit Information Technology{" "}
+              <span className=" font-normal">02/2019 - 12/2020</span>
+            </h3>
+            <p>Frontend Engineer</p>
+          </div>
+          <div className="mt-2 text-lg">
+            <ul className="list-disc pl-8">
+              <li>
+                Use React, React-redux and Redux-observable for front-end
+                development and collaborate with the back-end team to complete 4
+                projects through git-flow.
+              </li>
+              <li>
+                Upgraded React16 to React17, rewriting class base components to
+                function base and hooks, reducing code size by 30% and
+                increasing productivity
+              </li>
+              <li>
+                Increase project performance by refactoring code with better
+                rendering practices.
+              </li>
+              <li>
+                Contribute to the {`company's`} internal UI library, making 30+
+                pull requests and writing tests for components with jest.
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </main>
