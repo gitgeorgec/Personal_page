445d65d31989f134b0d27aeb5cb7f1bf833f8462
george <ccchang0661@gmail.com>
2024-08-17 09:07:12 +0800

    UPD: update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/Personal_page" : "",
   output: "export",
 };
 
