eac226e80aad31346534ff051e0fd6d1822f1c9c
george <ccchang0661@gmail.com>
2024-08-17 08:52:26 +0800

    UPD: update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProd ? "/Personal_page" : "",
   output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
